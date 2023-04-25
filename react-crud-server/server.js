ff247a595eeaec166c757193b6f6f5c3a4d9feee
rasel497 <rasel.csediu44@gmail.com>
2023-04-25 11:36:33 +0600

    mysql db connect with sever side and client side

---
+++ react-crud-server/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+// connect mysql db
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'react_mysql-crud'
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, result) => {
+        if (err)
+            return res.json({ Message: "Error inside server" });
+        return res.json(result);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening.... on port ${port}`)
+});
